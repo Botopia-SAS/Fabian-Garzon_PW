1f1388910f45db2996d653a32a0c20eb36ac9aa4
Botopia-SAS <botopia.sas@gmail.com>
2025-01-27 16:12:55 -0500

    Se agrega el contenido de prints que hacía falta en al último push

---
+++ app/Prints/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Prints from "@/components/prints"; // Asegúrate de que la ruta sea correcta
+
+const Page = () => {
+  return (
+    <div>
+      <Prints />
+    </div>
+  );
+};
+
+export default Page;
---
+++ components/prints.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+
+const Prints = () => {
+  return (
+    <section className="pt-40 flex justify-center">
+      <div className="max-w-2xl text-left space-y-6">
+        <h1 className="text-2xl font-bold">Limited Edition Appeal</h1>
+        <p className="text-base leading-relaxed">
+          Transform Your Space with Exclusive Fine Art Prints
+        </p>
+        <p className="text-base leading-relaxed">
+          <br></br>
+          <br></br>
+          Immerse yourself in the artistry of Fabian Garzon. Explore a curated
+          collection of limited-edition photography prints that blend
+          sophistication with timeless beauty. Each piece tells a story, evoking
+          emotion and inspiring awe. Don’t miss your chance to own a signed and
+          numbered artwork.
+        </p>
+        <p className="text-base leading-relaxed font-semibold">
+          <br></br>
+          <br></br>
+          Shop Now and Elevate Your Space
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Prints;
